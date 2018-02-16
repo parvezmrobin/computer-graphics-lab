19de41544ba6bc26d40b5399929f0d42ae74687e
Parvez M Robin <parvezmrobin@gmail.com>
2018-02-16 12:40:17 +0600

    line scan

--- routes/scan.js
+++ routes/scan.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
 router.get('/circle', function(req, res, next) {
     res.render('scan/circle', { title: 'Bresenham’s Circle Drawing' });
+});
+
+router.get('/line', function(req, res, next) {
+    res.render('scan/line', { title: 'Bresenham’s Line Drawing' });
 });
 
 module.exports = router;
